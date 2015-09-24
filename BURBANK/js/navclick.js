2b19b3352c057eea62ffa98642ba778fcd4a6662
mariia-riabinina <mariia.riabinina@gmail.com>
2015-09-25 01:45:59 +0300

    delete burbank

--- BURBANK/js/navclick.js
+++
@@ -1,9 +0,0 @@
-function switchMenu() {
-    var nav = $("#nav");
-    var status = nav.css("display");
-    if (status == "block") {
-        nav.css("display", "none");
-    } else {
-        nav.css("display", "block");
-    }
-}
