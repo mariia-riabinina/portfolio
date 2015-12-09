a5508e74facc57eef7fc911ef3a1fbf0b972b053
mariia.riabinina <mariia.riabinina@gmail.com>
2015-12-09 17:08:48 +0200

    remove JustAuto
    remove JustAuto

--- JustAuto/js/arrow.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$("#arrow").click(function(event) {
-		event.preventDefault();
-		var id  = $(this).attr('href');
-		var height = $(id).offset().top;
-		$('body,html').animate({scrollTop: height}, 700);
-	});
-});
